66a469958e70031ffa25c40e622c937479983b03
anbarasu <anbarasu.ramu@gmail.com>
2022-07-28 15:06:51 +0530

    updates

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 	
 	@RequestMapping("/hello")
 	  public String hello() {
-	    return "Hello Anbarasu, this is change 2 in master branch!";
+	    return "Hi Minikube!";
 	  }
 	
 	
